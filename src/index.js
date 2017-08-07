cef090c57a4c73a4758182cb205e087d785d25c2
kitamura <kitamuradesu01@gmail.com>
2017-08-07 18:02:38 +0900

    es5からes6へ変換する

--- src/index.js
+++ src/index.js
@@ -2,43 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import './table.css';
 
-var Title = React.createClass({
-        render: function() {
-          return(
-              React.DOM.h1(null, "家計簿")
-            );
-        }
-});
+class TitleHeader extends Component {
+  render() {
+    return (
+      <h1>家計簿</h1>
+    );
+  }
+}
 
-var Excel = React.createClass({
-        displayName: 'Excel',
-        
-        propTypes: {
-          headers: React.PropTypes.arrayOf(
-            React.PropTypes.string
-          ),
-          initialData: React.PropTypes.arrayOf(
-            React.PropTypes.arrayOf(
-              React.PropTypes.any
-            )
-          ),
-        },
+class Excel extends Component {
 
-        getInitialState: function() {
-          return {
+  // Excel.propTypes = {
+  //   headers: PropTypes.arrayOf(PropTypes.string)
+  // };
+
+        getInitialState() {
+          return (
             data: this.props.initialData,
             sortby: null,
             descending: false,
             edit: null, // {row: 行番号, cell: 列番号}
-          };
+          );
         },
-        
-        _sort: function(e) {
-          var column = e.target.cellIndex;
-          var data = this.state.data.slice();
-          var descending = this.state.sortby === column && !this.state.descending;
+
+        _sort(e) {
+          let column = e.target.cellIndex;
+          let data = this.state.data.slice();
+          let descending = this.state.sortby === column && !this.state.descending;
           data.sort(function(a, b) {
-            return descending 
+            return descending
               ? (a[column] < b[column] ? 1 : -1)
               : (a[column] > b[column] ? 1 : -1);
           });
@@ -48,124 +40,97 @@ var Excel = React.createClass({
             descending: descending,
           });
         },
-        
-        _showEditor: function(e) {
+
+        _showEditor(e) {
           this.setState({edit: {
             row: parseInt(e.target.dataset.row, 10),
             cell: e.target.cellIndex,
           }});
         },
-        
-        _save: function(e) {
+
+        _save(e) {
           e.preventDefault();
-          var input = e.target.firstChild;
-          var data = this.state.data.slice();
+          let input = e.target.firstChild;
+          let data = this.state.data.slice();
           data[this.state.edit.row][this.state.edit.cell] = input.value;
           this.setState({
             edit: null,
             data: data,
           });
         },
-        
-        render: function() {
-          return (
-            React.DOM.table(null,
-              React.DOM.thead({onClick: this._sort},
-                React.DOM.tr(null,
-                  this.props.headers.map(function(title, idx) {
-                    if (this.state.sortby === idx) {
-                      title += this.state.descending ? ' \u2191' : ' \u2193'
-                    }
-                    return React.DOM.th({key: idx}, title);
-                  }, this)
-                )
-              ),
-              React.DOM.tbody({onDoubleClick: this._showEditor},
-                this.state.data.map(function(row, rowidx) {
-                  return (
-                    React.DOM.tr({key: rowidx},
-                      row.map(function(cell, idx) {
-                        var content = cell;
-                        var edit = this.state.edit;
-                        if (edit && edit.row === rowidx && edit.cell === idx) {
-                          content = React.DOM.form({onSubmit: this._save},
-                            React.DOM.input({
-                              type: 'text',
-                              defaultValue: cell,
-                            })
-                          );
-                        }
 
-                        return React.DOM.td({
-                          key: idx,
-                          'data-row': rowidx,
-                        }, content);
-                      }, this)
-                    )
-                  );
-                }, this)
-              )
-            )
+        render() {
+          return (
+      <table>
+        <thead onClick={this._sort}>
+          <tr>
+            {this.props.headers.map( (title, idx) => {
+              if ( this.state.sortby === idx) {
+                title += this.state.descending ? ' \u2191' : ' \u2193';
+              }
+              return (
+                <th key="idx">{title}</th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody onDoubleClick={this._showEditor}>
+        {this.state.data.map( (row, rowidx) => {
+          <tr key="rowidx">{title}</tr>)},
+
+              let content = cell;
+              let edit = this.state.edit;
+              if (edit && edit.row === rowidx && edit.cell === idx) {
+                content = React.DOM.form({onSubmit: this._save},
+                React.DOM.input({
+                type: 'text',
+                defaultValue: cell,
+                })
+              );
+            }
+
+            return <td key="idx">{rowidx}</td>
+
+        }
+
+        </tbody>
+      </table>
+          );
+        }
+      };
+
+class SogokeiKingaku extends Component({
+  render() {
+          return (
+            <table>
+              <thead>
+                <tr>
+                  <th>総合計</th>
+                  <th>{sogokei}</th>
+                </tr>
+              </thead>
+            </table>
           );
         }
       });
 
-var Button = React.createClass({
-    test: function(){
-    this.props.test(this.props.increment);
-    },
-    render: function(){
-        return (
-            <button onClick={this.test}>+{this.props.increment}</button>
-     );
-   }
-});
-
-var Result = React.createClass({
-  render: function(){
-    return(
-      <div>{this.props.localCounter}</div>
-    );
-  }
-});
-
-var Main = React.createClass({
-  getInitialState: function(){
-    return {counter: 0};
-  },
-  handleClick: function(increment){
-    this.setState({counter:this.state.counter+increment});
-  },
-  render: function(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <div>
-        <Button test={this.handleClick} increment={1} />
-        <Result localCounter={this.state.counter} />
+        <TitleHeader />
+        <Excel />
+        <SogokeiKingaku />
       </div>
     );
   }
-});
+}
 
-var Sogokei = React.createClass({
-  render: function() {
-          return (
-            React.DOM.table(null,
-              React.DOM.thead(null,
-                React.DOM.tr(null,
-                    React.DOM.th(null, "総合計"),
-                    React.DOM.th(null, sogokei)
-                )
-              )
-            )
-          );
-        }
-      });
 
-      
       var headers = [
         "ID", "品名", "単価", "数量", "合計"
       ];
-      
+
       var data = [
         //["", "", 0, 0, 0]
         ["", "", 0, 0, 0],
@@ -174,18 +139,7 @@ var Sogokei = React.createClass({
 
       var sogokei = 0;
 
-      ReactDOM.render(<Title />, document.getElementById("title_header"));
-      
       ReactDOM.render(
-        React.createElement(Excel, { headers: headers, initialData: data }),
-        document.getElementById("root")
+        <App />,
+      document.getElementById("root")
       );
-
-      ReactDOM.render(<Main />, document.getElementById("gyotsuika_btn"));
-      
-      ReactDOM.render(<Sogokei />, document.getElementById("sogokei_kingaku"));
-
-      //ReactDOM.render(
-        //React.createElement(Sogokei, { headers: headers, initialData: data }),
-        //<Sogokei />, document.getElementById("sogokei_kingaku")
-      //);
