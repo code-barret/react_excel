4f084abba7e1b19d4c5c365928f625282b200a7f
kitamura <kitamuradesu01@gmail.com>
2017-08-09 20:46:17 +0900

    動作確認完了

--- src/index.js
+++ src/index.js
@@ -1,46 +1,35 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-//import './table.css';
+import './table.css';
 
 class TitleHeader extends Component {
-  render() {
-    return (
-      <h1>家計簿</h1>
-    );
-  }
-}
+        render() {
+          return (
+            <h1>家計簿</h1>
+          );
+        }
+      }
 
 class Excel extends Component {
-
-  // Excel.propTypes = {
-  //   headers: PropTypes.arrayOf(PropTypes.string)
-  // };
-        //初期化
-        // getInitialState() {
-        //   return {
-        //     data: [],
-        //     sortby: null,
-        //     descending: false,
-        //     edit: null, // {row: 行番号, cell: 列番号}
-        //   }
-        // }
 
         //初期化
         constructor(props) {
         super(props);
-        //途中ここからやる
-          data: [],
+        this.state = {
+          data: data,
+          headers: headers,
           sortby: null,
           descending: false,
           edit: null, // {row: 行番号, cell: 列番号}
+        };
       }
 
 
-        _sort(e) {
-          let column = e.target.cellIndex;
+        _sort = (e) => {
+          let column = e.target.cellIndex; //ダブルクリックされた<td>要素を表す
           let data = this.state.data.slice();
           let descending = this.state.sortby === column && !this.state.descending;
-          data.sort(function(a, b) {
+          data.sort((a, b) => {
             return descending
               ? (a[column] < b[column] ? 1 : -1)
               : (a[column] > b[column] ? 1 : -1);
@@ -52,14 +41,15 @@ class Excel extends Component {
           });
         }
 
-        _showEditor(e) {
+        _showEditor = (e) => {
           this.setState({edit: {
             row: parseInt(e.target.dataset.row, 10),
             cell: e.target.cellIndex,
           }});
-        }
+          console.log(this.state.edit);
+        };
 
-        _save(e) {
+        _save = (e) => {
           e.preventDefault();
           let input = e.target.firstChild;
           let data = this.state.data.slice();
@@ -71,44 +61,53 @@ class Excel extends Component {
         }
 
         render() {
-          console.log(this.state.sortby);
+          //console.log(this.e);
+          console.log(this.content);
+          // console.log(data.map((datas, id) => datas[4]));
           return (
-      <table>
-        <thead onClick={this._sort}>
-          <tr>
-            {headers.map( (title) => {
-              if ( this.state.sortby === 123) {
-                title += this.state.descending ? ' \u2191' : ' \u2193';
-              }
-              return <th key="">{title}</th>;
-            })}
-          </tr>
-        </thead>
-        <tbody onDoubleClick={this._showEditor}>
-        {this.state.data.map( (row, rowidx) => {
-          <tr key="rowidx">{}</tr>;
+            <table>
+              <thead onClick={this._sort}>
+                <tr>
+                  {headers.map( (title, idx) => {
+                    if ( this.state.sortby === idx) {
+                      title += this.state.descending ? ' \u2191' : ' \u2193';
+                    }
+                    return <th key={idx}>{title}</th>;
+                  })}
+                </tr>
+              </thead>
+              <tbody onDoubleClick={this._showEditor}>
+              {this.state.data.map( (row, rowidx) => {
+                return(
+                <tr key={rowidx}>{row.map((cell, idx) => {
+                  let content = cell;
+                  let edit = this.state.edit;
 
-              let content = [];
-              let edit = this.state.edit;
-              if (edit && edit.row === rowidx && edit.cell === []) {
-                <form onSubmit={this.handlesubmit}>
-                  <label>
-                    Name:
-                      <input type="text" value="" />
-                  </label>
-                  <input type="submit" value="this._save" />
-                </form>
+                  if (edit && edit.row === rowidx && edit.cell === idx) {
+
+                     content = ( <form onSubmit={this._save}>
+                          <input type="text" defaultValue={this.state.cell} />
+                      </form>
+                    )
                 }
 
-            return <td key="">{rowidx}</td>;
-          })
-        }
+                    //idxとrowidxがeditプロパティの値と一致する場合、contentを
+                    //入力フィールドに置き換えます。そうでない場合は、文字列をそのまま表示します
+                    return <td key={idx} data-row={rowidx}>{content}</td>
+                  })}
+                </tr>
+               );
+              })}
 
-        </tbody>
-      </table>
-          );
-        }
-      };
+            </tbody>
+          </table>
+        );
+      }
+    };
+
+  // Excel.propTypes = {
+  //   headers: PropTypes.arrayOf(PropTypes.string)
+  // };
 
 class SogokeiKingaku extends Component{
   render() {
@@ -142,11 +141,13 @@ class App extends Component {
         "ID", "品名", "単価", "数量", "合計"
       ];
 
-      var data = [
-        //["", "", 0, 0, 0]
-        ["", "", 0, 0, 0],
-        ["", "", 0, 0, 0]
-      ];
+      // var data = [
+      //   //["", "", 0, 0, 0]
+      //   ["", "", 0, 0, 0],
+      //   ["", "", 0, 0, 0],
+      //   ["", "", 0, 0, 0]
+      // ];
+      var data = [...Array(5).keys()].map(i => [i + 1, "", 0, 0, 0]);
 
       var sogokei = 0;
 
