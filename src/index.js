0ed13ee842a0c0f1f28139a3b97b2bf73672f0a1
kitamuradesu <kitamuradesu01@gmail.com>
2017-08-14 05:52:27 +0900

    マテリアル適用

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './table.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
 class TitleHeader extends Component {
         render() {
           return (
-            <h1>家計簿</h1>
+            <MuiThemeProvider>
+            <AppBar
+              title="家計簿"
+              iconClassNameRight="muidocs-icon-navigation-expand-more" />
+              </MuiThemeProvider>
           );
         }
       }
@@ -18,12 +35,12 @@ class Excel extends Component {
         this.state = {
           data: data,
           headers: headers,
+          total_price: total_price,
           sortby: null,
           descending: false,
           edit: null, // {row: 行番号, cell: 列番号}
         };
       }
-
 
         _sort = (e) => {
           let column = e.target.cellIndex; //ダブルクリックされた<td>要素を表す
@@ -46,7 +63,6 @@ class Excel extends Component {
             row: parseInt(e.target.dataset.row, 10),
             cell: e.target.cellIndex,
           }});
-          // console.log(this.state.edit);
         };
 
         _save = (e) => {
@@ -61,26 +77,30 @@ class Excel extends Component {
         };
 
         render() {
-          //console.log(this.e);
-          // console.log(this.content);
-          // console.log(this.state.total_price);
-          // console.log(data.map((datas, id) => datas[4]));
+
+          this.state.data.forEach((a,b,c) => {
+            data[b][4] = a[2] * a[3];
+            console.log(data[b][4]);
+            //total_price += data[b][4];
+          });
+
           return (
+            <MuiThemeProvider>
             <table>
-              <thead onClick={this._sort}>
-                <tr>
+              <TableHeader onClick={this._sort}>
+                <TableRow>
                   {headers.map( (title, idx) => {
                     if ( this.state.sortby === idx) {
                       title += this.state.descending ? ' \u2191' : ' \u2193';
                     }
-                    return <th key={idx}>{title}</th>;
+                    return <TableHeaderColumn key={idx}>{title}</TableHeaderColumn>;
                   })}
-                </tr>
-              </thead>
-              <tbody onDoubleClick={this._showEditor}>
+                </TableRow>
+              </TableHeader>
+              <TableBody onDoubleClick={this._showEditor}>
               {this.state.data.map( (row, rowidx) => {
                 return(
-                <tr key={rowidx}>{row.map((cell, idx) => {
+                <TableRow key={rowidx}>{row.map((cell, idx) => {
                   let content = cell;
                   let edit = this.state.edit;
 
@@ -94,14 +114,15 @@ class Excel extends Component {
 
                     //idxとrowidxがeditプロパティの値と一致する場合、contentを
                     //入力フィールドに置き換えます。そうでない場合は、文字列をそのまま表示します
-                    return <td key={idx} data-row={rowidx}>{content}</td>
+                    return <TableRowColumn key={idx} data-row={rowidx}>{content}</TableRowColumn>
                   })}
-                </tr>
+                </TableRow>
                );
               })}
 
-            </tbody>
+            </TableBody>
           </table>
+          </MuiThemeProvider>
         );
       }
     };
@@ -111,57 +132,72 @@ class Excel extends Component {
   // };
 
 class AddRow extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: data,
-		};
-	}
 
-	addrow = () => {
+  addrow = () => {
 
-		// add = [this.state.data, 6]
-		// this.setState({data: add});
-		// console.log(add);
+    // add = [this.state.data, 6]
+    // this.setState({data: add});
+    // console.log(add);
 
-		// let add = update(this.state.data, {$push: [5]});
-		// console.log(add);
-		// this.setState({data: add});
+    // let add = update(this.state.data, {$push: [5]});
+    // console.log(add);
+    // this.setState({data: add});
 
-	// 	this.setState({ data: this.state.data.map(i => {
-	// 		i.length += 1;
-	// 		console.log(i);
-	// 	})
-	// });
-		console.log("プッシュ");
-	}
+  //  this.setState({ data: this.state.data.map(i => {
+  //    i.length += 1;
+  //    console.log(i);
+  //  })
+  // });
+    console.log("プッシュ");
+  }
 
-	render() {
-		return(
-			<button onClick={this.addrow}>行追加</button>
-		);
-	}
+  render() {
+    return(
+      <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+        <RaisedButton label="行追加" onClick={this.addrow}　/>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 class Total_price_table extends Component{
-
-
+  constructor(props) {
+    super(props);
+  }
 
   render() {
-  	// let calc_price = data[0][2] + data[1][2] + data[2][2] + data[3][2];
-  	let calc_price = data.reduce(function(a,b){
-  		//console.log(a[2].concat(b[2]));
-  		return c[a].concat(b[2]);
-  	});
+    //let calc_price = data[0][2] + data[1][2] + data[2][2] + data[3][2];
+    // let calc_price = this.state.data.reduce((a,b) => {
+   //    if(a.length === b.length) {
+   //       return a[2] * b[3];
+   //    //  console.log(a[2], b[3]);
+   //    }
+
+   //    else {
+   //      a = [0, "", a, 0, 0];
+   //      console.log(a[2], b[3]);
+   //       return a[2] * b[3];
+   //    }
+    // }, this.state.data[0][2]);
+    //let calc_price = data.slice();
+    data.forEach((a, b, c) => {
+      //data[b][4] = a[4] * a[3];
+      total_price += a[4];
+      //total_price += data[b][4];
+      //console.log(data[b][4]);
+    });
+
           return (
+          <MuiThemeProvider>
             <table>
-              <thead>
-                <tr>
-                  <th>総合計</th>
-                  <th>{calc_price}</th>
-                </tr>
-              </thead>
+              <TableHeader>
+                <TableRow>
+                  <TableRowColumn>総合計</TableRowColumn>
+                  <TableRowColumn>{total_price}</TableRowColumn>
+                </TableRow>
+              </TableHeader>
             </table>
+          </MuiThemeProvider>
           );
         }
       }
@@ -190,7 +226,7 @@ class App extends Component {
       //   ["", "", 0, 0, 0],
       //   ["", "", 0, 0, 0]
       // ];
-      var data = [...Array(5).keys()].map(i => [i + 1, "", 1, 2, 0]);
+      var data = [...Array(5).keys()].map(i => [i + 1, "", i + 1, 2, 0]);
 
       var total_price = 0;
 
