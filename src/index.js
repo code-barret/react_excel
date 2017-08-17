37e665a2569ef15cac4d0ec71bd8c8710e0e7faf
kitamura <kitamuradesu01@gmail.com>
2017-08-17 17:38:59 +0900

    削除完了？

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ class Excel extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [...Array(5).keys()].map(i => [ i + 1, "", i + 1, 2, 0]),
+      data: [...Array(5).keys()].map(i => [ i + 1, "", i + 1, 2, 0, ""]),
       sortby: null,
       descending: false,
       edit: null, // {row: 行番号, cell: 列番号}
@@ -40,9 +40,9 @@ class Excel extends Component {
   }
 
   _sort = (e) => {
-    let column = e.target.cellIndex; //ダブルクリックされた<td>要素を表す
-    let data = this.state.data.slice();
-    let descending = this.state.sortby === column && !this.state.descending;
+    const column = e.target.cellIndex; //ダブルクリックされた<td>要素を表す
+    const data = this.state.data.slice();
+    const descending = this.state.sortby === column && !this.state.descending;
     data.sort((a, b) => {
       return descending
         ? (a[column] < b[column] ? 1 : -1)
@@ -62,22 +62,34 @@ class Excel extends Component {
 
   _save = (e) => {
     e.preventDefault();
-    let input = e.target.firstChild;
-    let data = this.state.data.slice();
+    const input = e.target.firstChild;
+    const data = this.state.data.slice();
     data[this.state.edit.row][this.state.edit.cell] = input.value;
     this.setState({edit: null, data: data});
   }
 
   addRow = () => {
-    //let addrow = this.state.data.concat(newrow);
-    console.log('プッシュ');
+    //const add = [this.state.data.length + 1, "", 1, 2];
+    const addrow = [...this.state.data, [this.state.data.length + 1, "", 1, 2, 0, ""]];
+    this.setState({data: addrow});
+  }
+
+  delete = () => {
+    //console.log(this.state.edit.row);
+    const deleteNumber = this.state.data.filter(d => {
+      console.log(d[0]);
+      //console.log(this.state.edit.row);
+      return d[0] !== this.state.edit.row;
+    });
+    this.setState({data: deleteNumber});
   }
 
   render() {
-    let headers = ["ID", "品名", "単価", "数量", "合計"];
+    const headers = ["ID", "品名", "単価", "数量", "合計"];
     this.state.data.forEach((d, i) => this.state.data[i][4] = d[2] * d[3] );
-    //this.setState({data: this.state.data.map((d,i) => d[4] = d[2] * d[3])});
-    let total_price = this.state.data.map( a => a[4]).reduce((p, c) => p + c);
+    //this.setState({data: this.state.data.map( d => d[4] = d[2] * d[3])});
+    const total_price = this.state.data.map( a => a[4]).reduce((p, c) => p + c);
+    //console.log(this.state.edit);
 
     return (
       <div>
@@ -101,19 +113,21 @@ class Excel extends Component {
               {this.state.data.map((row, rowidx) => {
                   return (
                     <TableRow key={rowidx}>{row.map((cell, idx) => {
-                        let content = cell;
-                        let edit = this.state.edit;
-
-                        let submit = (edit && edit.row === rowidx && edit.cell === idx)
-                        ?  content = (
-                            <form onSubmit={this._save}>
-                              <input type="text" defaultValue={this.state.cell}/>
-                            </form>
-                           )
+                        //const content = cell;
+                        const edit = this.state.edit;
+                          //console.log(cell, idx);
+                        const content = (edit && edit.row === rowidx && edit.cell === idx)
+                        ? <form onSubmit={this._save}>
+                            <input type="text" defaultValue={this.state.cell}/>
+                          </form>
                         : cell;
 
+                        const btn = (idx === 5)
+                        ? <button onClick={this.delete}>{"削除"}</button>
+                        : null;
+
                         //idxとrowidxがeditプロパティの値と一致する場合、contentを 入力フィールドに置き換えます。そうでない場合は、文字列をそのまま表示します
-                        return <TableRowColumn key={idx} data-row={rowidx}>{content}</TableRowColumn>
+                        return <TableRowColumn key={idx} data-row={rowidx}>{content}{btn}</TableRowColumn>
                       })}
                     </TableRow>
                   );
@@ -122,7 +136,9 @@ class Excel extends Component {
             </TableBody>
           </Table>
         </MuiThemeProvider>
-        <AddRow addRow={this.addRow}/>
+        <MuiThemeProvider>
+          <RaisedButton label="行追加" onClick={this.addRow}/>
+        </MuiThemeProvider>
         <Total_price_table total_price={total_price}/>
       </div>
     );
@@ -130,23 +146,6 @@ class Excel extends Component {
 };
 
 // Excel.propTypes = {   headers: PropTypes.arrayOf(PropTypes.string) };
-
-class AddRow extends Component {
-
-  //addrow = () => {
-    // this.setState({data: add});  this.setState({ data: this.state.data.map(i => {
-    //    i.length += 1;    console.log(i);  }) });
-    //console.log('プッシュ');
-  //}
-
-  render() {
-    return (
-      <MuiThemeProvider>
-        <RaisedButton label="行追加" onClick={this.props.addRow}/>
-      </MuiThemeProvider>
-    );
-  }
-}
 
 class Total_price_table extends Component {
   render() {
