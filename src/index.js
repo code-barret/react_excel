8edd026cf47efeaba35a89941f94950869594b48
kitamuradesu <kitamuradesu01@gmail.com>
2017-08-21 03:14:01 +0900

    mapに変更、0の場合falseになる完了

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import AppBar from 'material-ui/AppBar';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -50,7 +51,8 @@ class Excel extends Component {
 
   _showEditor = (e) => {
     //console.log(e.target.dataset.row);
-    console.log(e.currentTarget);
+    console.log(e.target.dataset.row);
+    console.log(e.target.cellindex);
     this.setState({
       edit: {
         row: parseInt(e.target.dataset.row, 10),
@@ -74,8 +76,8 @@ class Excel extends Component {
   }
 
   delete = (event) => {
-    console.log(event.target.dataset.id);
-    console.log(event.currentTarget);
+    //console.log(event.target.dataset.id);
+    //console.log(event.currentTarget);
     const deleteNumber = this.state.data.filter(d => {
       //console.log(this.state.edit.row);
       return d[0] !== parseInt(event.target.dataset.id);
@@ -87,9 +89,9 @@ class Excel extends Component {
     const headers = ["ID", "品名", "単価", "数量", "合計", "削除"];
     // this.state.data.forEach((d, i) => this.state.data[i][4] = d[2] * d[3] );
     // console.log(this.state.data);
-    this.setState({data: this.state.data.map( d => [ d[0], d[1], d[2], d[3], [...d[4] = d[2] * d[3]], d[5] ] )});
-    console.log(this.state.data);
-    const total_price = this.state.data.map( a => a[4]).reduce((p, c) => p + c);
+    const _data =  this.state.data.map( d => [ d[0], d[1], d[2], d[3], d[2] * d[3], d[5] ]);
+    //console.log(this.state.data);
+    const total_price = _data.map( a => a[4]).reduce((p, c) => p + c);
     //console.log(this.state.edit);
 
     return (
@@ -107,24 +109,31 @@ class Excel extends Component {
               </TableRow>
             </TableHeader>
             <TableBody
-              showRowHover={true}
+              
               stripedRows={true}
               displayRowCheckbox={false}
               onDoubleClick={this._showEditor}>
-              {this.state.data.map((row, rowidx) => {
+              {_data.map((row, rowidx) => {
                   return (
                     <TableRow key={rowidx}>{row.map((cell, idx) => {
                         //const content = cell;
                         const edit = this.state.edit;
                           //console.log(cell, idx);
-                        const content = (edit && edit.row && edit.row === rowidx && edit.cell === idx && idx < 4)
+                        const content = (edit  && edit.row === rowidx && edit.cell === idx && idx < 4)
                         ? <form onSubmit={this._save}>
                             <input type="text" defaultValue={this.state.cell}/>
                           </form>
                         : cell;
 
                         const btn = (idx === 5)
-                        ? <button onClick={this.delete} data-id={this.state.data[rowidx][0]}>{"削除"}</button>
+                        ? <FlatButton 
+                            onClick={this.delete}
+                            data-id={row[0]}
+                            backgroundColor="LightGrey"
+                            hoverColor="red"
+                            label="削除"
+                          >
+                          </FlatButton>
                         : null;
 
                         //idxとrowidxがeditプロパティの値と一致する場合、contentを 入力フィールドに置き換えます。そうでない場合は、文字列をそのまま表示します
@@ -152,11 +161,28 @@ class Total_price_table extends Component {
   render() {
     return (
       <MuiThemeProvider>
-        <Table>
+        <Table 
+          style={{
+            width: '40%',
+            border: '2px solid #CCF1F6',
+            margin: '0 auto',
+          }}
+        >
           <TableBody displayRowCheckbox={false}>
             <TableRow>
-              <TableRowColumn>総合計</TableRowColumn>
-              <TableRowColumn>{this.props.total_price}</TableRowColumn>
+              <TableRowColumn 
+                style={{
+                  backgroundColor: '#CCF1F6',
+                  textAlign: 'center',
+                }}
+              > 総合計
+              </TableRowColumn>
+              <TableRowColumn 
+                style={{
+                  textAlign: 'right',
+                }}
+              > {this.props.total_price}
+            　</TableRowColumn>
             </TableRow>
           </TableBody>
         </Table>
