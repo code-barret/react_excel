04559babeffa0e0c50ce1e178b17d6b1d7cdd4eb
kitamura <kitamuradesu01@gmail.com>
2017-08-28 20:44:22 +0900

    完了

--- src/index.test.js
+++ src/index.test.js
@@ -70,49 +70,48 @@ const editReducer = (state = [], action) => {
     }
   }
 
-//     //完了
-// describe("dataReducer", () => {
-//   it("ADD_ROW", () => {
-//     const result = dataReducer(dataInitialState, {type: "ADD_ROW"});
-//     const expected = [...Array(6).keys()].map(i => [ i + 1, "未入力", 1, 2, 0, ""]);
+    //完了
+describe("dataReducer", () => {
+  it("ADD_ROW", () => {
+    const result = dataReducer(dataInitialState, {type: "ADD_ROW"});
+    const expected = [...Array(6).keys()].map(i => [ i + 1, "未入力", 1, 2, 0, ""]);
 
-//     expect(result).toEqual(expected);
-//   });
-// });
+    expect(result).toEqual(expected);
+  });
+});
 
-//     // 完了
-  // it("DELETE", () => {
-  //   const result = dataReducer(dataInitialState, { type: "DELETE", eventTargetId: 1});
-  //   const expected = [...Array(4).keys()].map(i => [ i + 2, "未入力", 1, 2, 0, ""]);
+    // 完了
+  it("DELETE", () => {
+    const result = dataReducer(dataInitialState, { type: "DELETE", eventTargetId: 1});
+    const expected = [...Array(4).keys()].map(i => [ i + 2, "未入力", 1, 2, 0, ""]);
 
-  //   expect(result).toEqual(expected);
-  // });
+    expect(result).toEqual(expected);
+  });
 
-// });
-//     //完了
-// describe("editReducer", () => {
-//   it("SHOW_EDITOR", () => {
-//     const result = editReducer([], {type: "SHOW_EDITOR", row: 1, cell: 2});
-//     const expected = {row: 1, cell: 2};
+    //完了
+describe("editReducer", () => {
+  it("SHOW_EDITOR", () => {
+    const result = editReducer([], {type: "SHOW_EDITOR", row: 1, cell: 2});
+    const expected = {row: 1, cell: 2};
 
-//     expect(result).toEqual(expected);
-//   });
-// });
-      //完了
-// describe("dataReducer",  () => {
-//   it("SAVE", () => {
-//     const result = dataReducer(dataInitialState, {type: "SAVE", input: 32, row: 2, cell: 1});
-//     const expected = [
-//       [ 1, '未入力', 1, 2, 0, '' ],
-//       [ 2, '未入力', 1, 2, 0, '' ],
-//       [ 3, 32, 1, 2, 0, '' ],
-//       [ 4, '未入力', 1, 2, 0, '' ],
-//       [ 5, '未入力', 1, 2, 0, '' ] ];
+    expect(result).toEqual(expected);
+  });
+});
+      完了
+describe("dataReducer",  () => {
+  it("SAVE", () => {
+    const result = dataReducer(dataInitialState, {type: "SAVE", input: 32, row: 2, cell: 1});
+    const expected = [
+      [ 1, '未入力', 1, 2, 0, '' ],
+      [ 2, '未入力', 1, 2, 0, '' ],
+      [ 3, 32, 1, 2, 0, '' ],
+      [ 4, '未入力', 1, 2, 0, '' ],
+      [ 5, '未入力', 1, 2, 0, '' ] ];
 
-//     expect(result).toEqual(expected);
-//   });
-// });
-      //完了
+    expect(result).toEqual(expected);
+  });
+});
+//       完了
 // describe("editReducer", () => {
 //   it("SHOW_EDITOR", () => {
 //     const result = editReducer([], {type: "SHOW_EDITOR", row: null, cell: null});
@@ -122,26 +121,26 @@ const editReducer = (state = [], action) => {
 //   });
 // });
 
-//  describe("dataReducer", () => {
-//    it("SORT_DATA", () => {
-//      const result = dataReducer(sortDataInitial, {type: "SORT_DATA", sortby: 0, column: 0, descending: false});
-//      const expected = //[
-//       // [ 1, '未入力', 30, 23, 0, '' ],
-//       // [ 2, '未入力', 10, 21, 0, '' ],
-//       // [ 3, '未入力', 20, 22, 0, '' ],
-//       // [ 4, '未入力', 40, 24, 0, '' ],
-//       // [ 5, '未入力', 50, 25, 0, '' ] ];
+ describe("dataReducer", () => {
+   it("SORT_DATA", () => {
+     const result = dataReducer(sortDataInitial, {type: "SORT_DATA", sortby: 0, column: 0, descending: false});
+     const expected = //[
+      // [ 1, '未入力', 30, 23, 0, '' ],
+      // [ 2, '未入力', 10, 21, 0, '' ],
+      // [ 3, '未入力', 20, 22, 0, '' ],
+      // [ 4, '未入力', 40, 24, 0, '' ],
+      // [ 5, '未入力', 50, 25, 0, '' ] ];
 
-//       [
-//         [ 5, '未入力', 50, 25, 0, '' ],
-//         [ 4, '未入力', 40, 24, 0, '' ],
-//         [ 3, '未入力', 20, 22, 0, '' ],
-//         [ 2, '未入力', 10, 21, 0, '' ],
-//         [ 1, '未入力', 30, 23, 0, '' ] ];
+      [
+        [ 5, '未入力', 50, 25, 0, '' ],
+        [ 4, '未入力', 40, 24, 0, '' ],
+        [ 3, '未入力', 20, 22, 0, '' ],
+        [ 2, '未入力', 10, 21, 0, '' ],
+        [ 1, '未入力', 30, 23, 0, '' ] ];
 
-//      expect(result).toEqual(expected);
-//    });
-//  });
+     expect(result).toEqual(expected);
+   });
+ });
 
 describe("descendingReducer", () => {
   it("SORT_DESCENDING", () => {
